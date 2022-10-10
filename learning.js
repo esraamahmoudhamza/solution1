6bd6bf7a9d90053c78a7349eca42264894c779b6
esraamahmoudhamza <111077862+esraamahmoudhamza@users.noreply.github.com>
2022-10-10 18:29:34 +0200

    Add files via upload

---
+++ learning.js
@@ -0,0 +1,5 @@
+let names=['Ahmed','Aly','Jaser','Jasmin','Gamal','Salem','Esraa'];
+names=['Jaser','Jasmen']?
+    document.write('goodbye Jaser , goodbye Jasmin')
+    : document.write('Hello someone' );
+
